821ef795d533f6979c058517b060d76c9f747bd3
tiagorsdfonseca <tiagorsdfonseca@gmail.com>
2026-05-06 14:01:32 -0400

    Tests to read account added

--- service/routes.py
+++ service/routes.py
@@ -69,6 +69,22 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_account(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based on an id given as argument
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+
+    #check_content_type("application/json")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not been found.")
+    
+    # Uncomment once get_accounts has been implemented
+    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
