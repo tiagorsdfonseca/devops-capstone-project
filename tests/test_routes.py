e0ce4b6a2f75075b5d8497e74ca6a8b3d2df4182
tiagorsdfonseca <tiagorsdfonseca@gmail.com>
2026-05-06 15:29:43 -0400

    Tests to update account added

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,4 +138,18 @@ class TestAccountService(TestCase):
         """It should search for an account and find out if the account exists"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_update_account(self):
+        """It should update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
